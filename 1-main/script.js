d8032cc41686e77eb1e03f27c6f5582b649c1118
mauricio9002 <mauricio.golemba@escola.pr.gov.br>
2025-08-13 09:03:43 -0300

    Delete 1-main directory

--- 1-main/script.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById('contactForm').addEventListener('submit', function(event){
-    if (!document.getElementById('dataConsent').checked) {
-        event.preventDefault();
-        alert('você deve concordar com a politica de privacidade e o uso e dados conforme a lgpd.')
-    }
-})
